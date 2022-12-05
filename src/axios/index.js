61eef7f2e2f6847abb74de1927e0e77832ade405
srikalyan r <srikalyan.rs@gmail.com>
2022-12-05 16:28:31 +0530

    Change Url to Hosted backend

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+const URL = "https://wild-blue-seal-toga.cyclic.app";
+// const localhost = "http://localhost:8080";
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: URL,
   timeout: 1000,
 });
